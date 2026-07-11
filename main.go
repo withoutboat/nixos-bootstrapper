404d0b75d90b0105895379dfbfe8df4e6eb038d5
Vladimir Markov <49807858+withoutboat@users.noreply.github.com>
2026-07-11 15:31:30 +0500

    feat: update BuildDate to version 11

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ import (
 	"golang.org/x/crypto/argon2"
 )
 
-var BuildDate = "version 10 (Multi-EFI / XBOOTLDR)"
+var BuildDate = "version 11 (Multi-EFI / XBOOTLDR)"
 
 var (
 	titleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("#00F5D4")).Bold(true).MarginLeft(2)
